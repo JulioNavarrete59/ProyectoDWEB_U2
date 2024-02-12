48ed0356c459f660158f7666e8c89f1158773de0
jssanchez03 <jssanchez9@espe.edu.ec>
2024-02-12 14:42:51 -0500

    cimplementación de managedbean empleado

---
+++ src/main/java/com/concesionaria/controlador/EmpleadosController.java
@@ -0,0 +1,50 @@
+package com.concesionaria.controlador;
+
+
+import com.concesionaria.dao.EmpleadosImpl;
+import com.concesionaria.idao.IEmpleadosDao;
+import com.concesionaria.modelo.empleados;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+
+import java.util.List;
+import java.util.Map;
+
+@RequestScoped
+@Named(value = "empleadosBean")
+public class EmpleadosController {
+
+    IEmpleadosDao empleadosDao = new EmpleadosImpl();
+    private empleados empleado = new empleados();
+    List<empleados> obtenerEmpleados() {
+        return empleadosDao.obtenerEmpleados();
+    }
+    public String editar(int id) {
+        empleados oEmpleados = new empleados();
+        oEmpleados = empleadosDao.obtenerEmpleadosPorId(id);
+        System.out.println(oEmpleados);
+        Map<String, Object> sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+        sessionMap.put("empleado", oEmpleados);
+        return "/editar.xhtml";
+    }
+    public String actualizar(empleados empleado) {
+        empleadosDao.modificar(empleado);
+        return "/index.xhtml";
+    }
+    public void eliminar(int id) {
+        empleadosDao.eliminar(id);
+        System.out.println("Empleado eliminado");
+    }
+    public String registrar(empleados empleado) {
+        empleadosDao.registrar(empleado);
+        return "/empleados/index.xhtml";
+    }
+    public empleados getEmpleado() {
+        return empleado;
+    }
+    public void setEmpleado(empleados empleado) {
+        this.empleado = empleado;
+    }
+
+}
